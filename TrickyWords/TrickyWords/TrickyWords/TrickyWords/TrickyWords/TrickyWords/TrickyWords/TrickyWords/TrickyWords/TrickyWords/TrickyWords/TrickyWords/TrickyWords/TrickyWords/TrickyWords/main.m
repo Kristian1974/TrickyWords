fbd6eda91bc9fea894b49968b5cbef48bfe4b169
Kristian1974 <kristian.mills@btinternet.com>
2013-11-04 11:06:03 +0000

    Initial Commit of App
    Initial Commit of App

---
+++ TrickyWords/TrickyWords/TrickyWords/TrickyWords/TrickyWords/TrickyWords/TrickyWords/TrickyWords/TrickyWords/TrickyWords/TrickyWords/TrickyWords/TrickyWords/TrickyWords/TrickyWords/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TrickyWords
+//
+//  Created by Kristian Mills on 30/10/2013.
+//  Copyright (c) 2013 Kristian Mills. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TWAppDelegate class]));
+    }
+}
